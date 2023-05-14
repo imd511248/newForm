dd2c0387894b4180b1533b14d09ae5d625e24ddb
Irshad <imd511248@gmail.com>
2023-05-14 09:24:09 +0530

    deploy form

---
+++ imd.js
@@ -0,0 +1,65 @@
+
+// function formSubmit() {
+//     let data1 = document.getElementById("data1")
+//     let data2 = document.getElementById("data2")
+//     let data3 = document.getElementById("data3")
+//     let data4 = document.getElementById("data4")
+//     let data5 = document.getElementById("data5")
+//     let data6 = document.getElementById("data6")
+//     let data7 = document.getElementById("data7")
+
+//     if (data1 == "") {
+//         document.getElementById("input1").style.display = "block"
+//     }
+//     else{
+//         document.getElementById("input1").style.display = "none"
+//     }
+
+
+//     if (data2 == "") {
+//         document.getElementById("input2").style.display = "block"
+//     }
+//     else{
+//         document.getElementById("input2").style.display = "none"
+//     }
+
+
+//     if (data3 == "") {
+//         document.getElementById("input3").style.display = "block"
+//     }
+//     else{
+//         document.getElementById("input3").style.display = "none"
+//     }
+
+
+//     if (data4 == "") {
+//         document.getElementById("input4").style.display = "block"
+//     }
+//     else{
+//         document.getElementById("input4").style.display = "none"
+//     }
+
+    
+//     if (data5 == "") {
+//         document.getElementById("input5").style.display = "block"
+//     }
+//     else{
+//         document.getElementById("input5").style.display = "none"
+//     }
+
+
+//     if (data6 == "") {
+//         document.getElementById("input6").style.display = "block"
+//     }
+//     else{
+//         document.getElementById("input6").style.display = "none"
+//     }
+
+
+//     if (data7 == "") {
+//         document.getElementById("input7").style.display = "block"
+//     }
+//     else{
+//         document.getElementById("input7").style.display = "none"
+//     }
+// }
